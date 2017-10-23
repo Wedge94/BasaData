794c5f8957ee2e998cc4a5743b3275fbf73216f2
wedge94 <ozowedge@gmail.com>
2017-10-23 05:43:39 +0200

    Baza

--- app/src/main/java/com/myapp/basadata/MainActivity.java
+++ app/src/main/java/com/myapp/basadata/MainActivity.java
@@ -1,14 +1,70 @@
 package com.myapp.basadata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd, btnViewData;
+    private EditText editText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.editText);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnViewData = (Button)findViewById(R.id.btnView);
+        mDatabaseHelper = new DatabaseHelper(this);
+        btnAdd.setOnClickListener(new View.OnClickListener()
+        {
+            public void  onClick(View v)
+            {
+                String newEntry = editText.getText().toString();
+                if(editText.length() != 0)
+                {
+                    AddData(newEntry);
+                    editText.setText("");
+                }
+                else
+                {
+                    toastMessage("You must put something in the text field!");
+                }
+            }
+        });
+
+        btnViewData.setOnClickListener(new View.OnClickListener()
+        {
+            public void  onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void AddData(String newEntry)
+    {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+        if(insertData)
+        {
+            toastMessage("Data Successfullt Inserted");
+        }
+        else
+        {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void toastMessage(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
-/* dupa */
